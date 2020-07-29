57949ed81f3ff670baebd4505b4596c679d9bd67
Hasten Bridges <hastenbridges@gmail.com>
2020-07-28 21:08:33 -0500

    Update windowaccess.lua

--- windowaccess.lua
+++ windowaccess.lua
@@ -1,24 +1,34 @@
 Citizen.CreateThread(function()
 
+
+
 while true do
     local playerPed = GetPlayerPed(-1)
     local playerVeh = GetVehiclePedIsIn(playerPed, false)
     Citizen.Wait(1)
     
-    --Rolls down windows upon switch to Franklin key press (Default F6)
 
+    --Rolls down windows upon switch to Franklin key press (Default F6)
     if IsControlJustReleased(1, 167) then
-        RollDownWindow(playerVeh, 0)
-        RollDownWindow(playerVeh, 1)
-        alert("~y~Windows rolled down!")
+        if IsPedInVehicle(playerPed, playerVeh, false) then
+            RollDownWindow(playerVeh, 0)
+            RollDownWindow(playerVeh, 1)
+            alert("~y~Windows rolled down!")
+
+        else alert("You must be in a vehicle to do this.")
+        end
     end
 
     --Rolls down windows upon switch to Trevor key press (Default F7)
 
     if IsControlJustReleased(1, 168) then
-        RollUpWindow(playerVeh, 0)
-        RollUpWindow(playerVeh, 1)
-        alert("~b~Windows rolled up!")
+        if IsPedInVehicle(playerPed, playerVeh, false) then
+            RollUpWindow(playerVeh, 0)
+            RollUpWindow(playerVeh, 1)
+            alert("~b~Windows rolled up!")
+
+        else alert("You must be in a vehicle to do this.")
+        end
     end
 end
 end)
@@ -29,7 +39,7 @@ end)
 function alert(msg)
     BeginTextCommandDisplayHelp("STRING")
     AddTextComponentSubstringPlayerName(msg)
-    EndTextCommandDisplayHelp(0, false, true, 3000)
+    EndTextCommandDisplayHelp(0, false, true, 2000)
 end
 
 --Made by Bridges
